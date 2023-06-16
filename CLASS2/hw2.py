1354bc974cc0bb73b02453a10509b9c66b1269d5
JAZAZJ <yuchunk1020@gmail.com>
2023-06-16 21:38:28 +0800

    hw2

--- CLASS2/hw2.py
+++ CLASS2/hw2.py
@@ -1,16 +1,12 @@
 '''
 week2 HW
 1. 請問Python有哪五種形態:
-答案:浮點=小數
-整數是INT
-布林是TRUE False
-字串是str
-2. 請問使用什麼function可以顯示出形態
-答案:
+答案:浮點=小數 整數是INT 布林是TRUE False 字串是str
+
 3. 請列出Python加、減、乘、除表示符號
-答案:
+答案:+ - * / //
 4. 請問今天學了哪一些function(函式)?
-答案:
+答案:max print min
 5. 延續上題, 請嘗試描述每個function的功能各別是什麼?
-答案:
+答案:max是取最大值 mix是取小值 print是列映
 '''
